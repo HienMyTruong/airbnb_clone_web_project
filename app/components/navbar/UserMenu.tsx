6cf378dc159e11af187ed336514ccae46f41bbf2
Hien Truong <hien.truong2002.ht@gmail.com>
2023-05-27 20:46:30 +0200

    added user menu

---
+++ app/components/navbar/UserMenu.tsx
@@ -0,0 +1,50 @@
+'use client';
+import { AiOutlineMenu } from 'react-icons/ai';
+
+const UserMenu = () => {
+    return (
+        <div className="relative">
+            <div
+                onClick={() => {}}
+                className="
+                    hidden
+                    md:block
+                    text-sm
+                    font-semibold
+                    py-3
+                    px-4
+                    rounded-full
+                    hover:bg-neutral-100
+                    transition
+                    cursor-pointer
+                "
+            >
+                Airbnb your home
+            </div>
+            <div
+                onClick={() => {}}
+                className="
+                    p-4
+                    md:py-1
+                    md:px-2
+                    border-[1px]
+                    flex
+                    flex-row
+                    items-center
+                    gap-3
+                    rounded-full
+                    cursor-pointer
+                    hover:shadow-md
+                    transition
+                "
+            
+            >
+                <AiOutlineMenu />
+
+            </div>
+
+        </div>
+    );
+}
+
+export default UserMenu; 
