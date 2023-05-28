4a982bb33ecc34f53843763a71f8710a5d65154e
Hien Truong <hien.truong2002.ht@gmail.com>
2023-05-28 08:41:41 +0200

    navbar ui fixed

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,55 +1,114 @@
 'use client';
 import { AiOutlineMenu } from 'react-icons/ai';
 import Avatar from '../Avatar';
+import MenuItem from './MenuItem';
+import { useCallback, useState } from 'react';
 
 const UserMenu = () => {
-    return (
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleOpen = useCallback(() => { setIsOpen((value) => !value); }, []);
+    return ( 
         <div className="relative">
-            <div
-                onClick={() => {}}
-                className="
-                    hidden
-                    md:block
-                    text-sm 
-                    font-semibold 
-                    py-3 
-                    px-4 
-                    rounded-full 
-                    hover:bg-neutral-100 
-                    transition 
-                    cursor-pointer
-                "
+          <div className="flex flex-row items-center gap-3">
+            <div 
+              onClick={() => {}}
+              className="
+                hidden
+                md:block
+                text-sm 
+                font-semibold 
+                py-3 
+                px-4 
+                rounded-full 
+                hover:bg-neutral-100 
+                transition 
+                cursor-pointer
+              "
             >
-                Airbnb your home
+              Airbnb your home
             </div>
-            <div
-                onClick={() => {}}
-                className="
-                    p-4
-                    md:px-2
-                    md:py-1
-                    border-[1px] 
-                    border-neutral-200 
-                    flex 
-                    flex-row 
-                    items-center 
-                    gap-3 
-                    rounded-full 
-                    cursor-pointer 
-                    hover:shadow-md 
-                    transition
-                "
-            
+            <div 
+            onClick={toggleOpen}
+            className="
+              p-4
+              md:py-1
+              md:px-2
+              border-[1px] 
+              border-neutral-200 
+              flex 
+              flex-row 
+              items-center 
+              gap-3 
+              rounded-full 
+              cursor-pointer 
+              hover:shadow-md 
+              transition
+              "
             >
-                <AiOutlineMenu />
-                <div className="hidden md:block"></div>
-                    <Avatar />
-                    
-
+              <AiOutlineMenu />
+              <div className="hidden md:block">
+                <Avatar />
+              </div>
             </div>
-
+          </div>
+          {isOpen && (
+            <div 
+              className="
+                absolute 
+                rounded-xl 
+                shadow-md
+                w-[40vw]
+                md:w-3/4 
+                bg-white 
+                overflow-hidden 
+                right-0 
+                top-12 
+                text-sm
+              "
+            >
+              <div className="flex flex-col cursor-pointer">
+                  <>
+                    <MenuItem 
+                      label="My trips" 
+                      onClick={() => {}}
+                    />
+                    <MenuItem 
+                      label="My favorites" 
+                      onClick={() => {}}
+                    />
+                    <MenuItem 
+                      label="My reservations" 
+                      onClick={() => {}}
+                    />
+                    <MenuItem 
+                      label="My properties" 
+                      onClick={() => {}}
+                    />
+                    <MenuItem 
+                      label="Airbnb your home" 
+                      onClick={() => {}}
+                    />
+                    <hr />
+                    <MenuItem 
+                      label="Logout" 
+                      onClick={() => {}}
+                    />
+                  </>
+                  <>
+                    <MenuItem 
+                      label="Login" 
+                      onClick={() => {}}
+                    />
+                    <MenuItem 
+                      label="Sign up" 
+                      onClick={() => {}}
+                    />
+                  </>
+              </div>
+            </div>
+          )}
         </div>
-    );
+       );
 }
 
 export default UserMenu; 
