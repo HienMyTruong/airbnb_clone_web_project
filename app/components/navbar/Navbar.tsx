443d00086dc5ef1939b0f9333b0b668c4816cb4f
Hien Truong <hien.truong2002.ht@gmail.com>
2023-05-31 17:32:30 +0200

    we are not passing through dangerous objects like date anymore with a wrapped type SafeUser instead of User. no more warning: Only plain objects can be passed to Client Components from Server Componenets. Date objects are not supported.

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -2,8 +2,16 @@ import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
+import { User } from "@prisma/client";
+import { SafeUser } from "@/app/types";
 
-const Navbar = () => {
+interface NavbarProps {
+    currentUser?: SafeUser | null;
+}
+const Navbar: React.FC<NavbarProps> = ({
+    currentUser,
+}) => {
+    //console.log({ currentUser })
     return (
         <div className="fixed w-full bg-white z-10 shadow-sm">
             <div
@@ -25,7 +33,7 @@ const Navbar = () => {
                         ">
                         <Logo />
                         <Search />
-                        <UserMenu />
+                        <UserMenu currentUser={currentUser} />
 
                     </div>
 
--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -5,9 +5,17 @@ import MenuItem from './MenuItem';
 import { useCallback, useState } from 'react';
 import useRegisterModal from '@/app/hooks/useRegisterModal';
 import useLoginModal from '@/app/hooks/useLoginModal';
+import { User } from "@prisma/client";
+import { signOut } from "next-auth/react";
+import { SafeUser } from '@/app/types';
 
 
-const UserMenu = () => {
+interface UserMenuProps {
+  currentUser?: SafeUser | null
+}
+const UserMenu: React.FC<UserMenuProps> = ({
+  currentUser
+}) => {
   const [isOpen, setIsOpen] = useState(false);
   const registerModal = useRegisterModal();
   const loginModal = useLoginModal();
@@ -72,43 +80,46 @@ const UserMenu = () => {
               "
         >
           <div className="flex flex-col cursor-pointer">
-            <>
-              <MenuItem
-                label="My trips"
-                onClick={() => { }}
-              />
-              <MenuItem
-                label="My favorites"
-                onClick={() => { }}
-              />
-              <MenuItem
-                label="My reservations"
-                onClick={() => { }}
-              />
-              <MenuItem
-                label="My properties"
-                onClick={() => { }}
-              />
-              <MenuItem
-                label="Airbnb your home"
-                onClick={() => { }}
-              />
-              <hr />
-              <MenuItem
-                label="Logout"
-                onClick={() => { }}
-              />
-            </>
-            <>
-              <MenuItem
-                label="Login"
-                onClick={loginModal.onOpen}
-              />
-              <MenuItem
-                label="Sign up"
-                onClick={registerModal.onOpen}
-              />
-            </>
+            {currentUser ? (
+              <>
+                <MenuItem
+                  label="My trips"
+                  onClick={() => { }}
+                />
+                <MenuItem
+                  label="My favorites"
+                  onClick={() => { }}
+                />
+                <MenuItem
+                  label="My reservations"
+                  onClick={() => { }}
+                />
+                <MenuItem
+                  label="My properties"
+                  onClick={() => { }}
+                />
+                <MenuItem
+                  label="Airbnb your home"
+                  onClick={() => { }}
+                />
+                <hr />
+                <MenuItem
+                  label="Logout"
+                  onClick={() => signOut()}
+                />
+              </>
+            ) : (
+              <>
+                <MenuItem
+                  label="Login"
+                  onClick={loginModal.onOpen}
+                />
+                <MenuItem
+                  label="Sign up"
+                  onClick={registerModal.onOpen}
+                />
+              </>
+            )}
           </div>
         </div>
       )}
